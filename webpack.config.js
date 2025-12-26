b6fee266d7a6164c5420146c27ac26c25b250b65
Primo <paoloalinab@gmail.com>
2025-12-26 12:34:43 +0800

    Add npm script shortcut to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -34,4 +34,9 @@ module.exports = {
       }
     ],
   },
+  "scripts": {
+    "build": "webpack --config webpack.prod.js",
+    "dev": "webpack serve --config webpack.dev.js",
+    "deploy": "git subtree push --prefix dist origin gh-pages"
+  }
 }
